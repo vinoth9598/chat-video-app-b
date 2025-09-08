41a9bf665e504ecb07f6adb9d608061d74d3bb10
vinoth9598 <pvinoth9598@gmail.com>
2025-09-08 12:33:17 +0530

    update

--- src/server.js
+++ src/server.js
@@ -13,10 +13,12 @@ import chatRouter from "./routes/chat.route.js" ;
 const port = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI 
 //middleware 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}));
+app.use(cors(
+    // {
+    // origin:"http://localhost:5173",
+    // credentials:true
+    // }
+));
 
 app.use(express.json()) ;
 app.use(cookieParser()) ;
